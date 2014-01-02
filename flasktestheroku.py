a74be72230c9c782dfef72750fd7838297551688
suxor <suxor@ich.ms>
2014-01-02 23:23:50 +0100

    Add CORS package

--- flasktestheroku.py
+++ flasktestheroku.py
@@ -1,16 +1,33 @@
 import os
 from flask import Flask
+import datetime
+try:
+  from flask_cors import cross_origin # support local usage without installed package
+except:
+  from flask.ext.cors import cross_origin # this is how you would normally import
 
 app = Flask(__name__)
+
 
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+@app.route('/time')
+@cross_origin()
+def printtime():
+    return str(datetime.datetime.now())
+
+#@app.route('')
+
 @app.route('/<parameter>')
 def parameter_return(parameter):
     return parameter
 
-#if __name__ == '__main__':
-#    app.run()
+
+
+#@app.route('/')
+
+if __name__ == '__main__':
+    app.run()
